1151cf6bb16f9a3ca059ca663c07dbb01453c3d5
acidtib <arubinofaux@gmail.com>
2025-01-04 01:42:09 -0700

    🚧 refactor: remove download progress logging from updater.ts

--- src/lib/updater.ts
+++ src/lib/updater.ts
@@ -1,5 +1,4 @@
 import { check } from '@tauri-apps/plugin-updater'
-import { relaunch } from '@tauri-apps/plugin-process'
 
 export async function checkForUpdates() {
   try {
@@ -9,29 +8,6 @@ export async function checkForUpdates() {
       console.log(
         `Found update ${update.version} from ${update.date} with notes: ${update.body}`
       )
-      
-      let downloaded = 0
-      let contentLength = 0
-      
-      await update.downloadAndInstall((event) => {
-        switch (event.event) {
-          case 'Started':
-            contentLength = event.data.contentLength || 0
-            console.log(`Started downloading ${event.data.contentLength} bytes`)
-            break
-          case 'Progress':
-            downloaded += event.data.chunkLength
-            const progress = contentLength > 0 ? (downloaded / contentLength) * 100 : 0
-            console.log(`Downloaded ${progress.toFixed(2)}%`)
-            break
-          case 'Finished':
-            console.log('Download finished')
-            break
-        }
-      })
-
-      console.log('Update installed, relaunching...')
-      await relaunch()
     } else {
       console.log('No updates available')
     }
