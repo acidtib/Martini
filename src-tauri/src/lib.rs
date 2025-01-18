c46cc3dad608fb6b79c1413d8b974e0bee217c9b
acidtib <arubinofaux@gmail.com>
2025-01-17 21:28:07 -0700

    ✨ feat: add SQL plugin and initial database migrations

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,17 +1,61 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use tauri::Manager;
+use tauri_plugin_sql::{Migration, MigrationKind};
 
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-pub mod shortcuts;
 pub mod screenshot;
+pub mod shortcuts;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let migrations = vec![
+        Migration {
+            version: 1,
+            description: "create settings table",
+            sql: r#"
+                    CREATE TABLE IF NOT EXISTS settings (
+                        key TEXT PRIMARY KEY,
+                        value TEXT NOT NULL
+                    );
+                "#,
+            kind: MigrationKind::Up,
+        },
+        Migration {
+            version: 2,
+            description: " Insert initial settings",
+            sql: r#"
+                    INSERT INTO settings (key, value) VALUES
+                        ('bootstrapped', 'false'),
+                        ('installed_on', CURRENT_TIMESTAMP);
+                "#,
+            kind: MigrationKind::Up,
+        },
+        Migration {
+            version: 3,
+            description: "create screenshots table",
+            sql: r#"
+                    CREATE TABLE IF NOT EXISTS screenshots (
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        image BLOB NOT NULL,
+                        filename TEXT,
+                        mime_type TEXT,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                    );
+                "#,
+            kind: MigrationKind::Up,
+        },
+    ];
+
     tauri::Builder::default()
+        .plugin(
+            tauri_plugin_sql::Builder::default()
+                .add_migrations("sqlite:app.db", migrations)
+                .build(),
+        )
         .setup(|app| {
             shortcuts::register_shortcuts(app)?;
             Ok(())
